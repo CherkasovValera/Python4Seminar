aea8c76e41a3cb1303f7c73df4aa8dc38cd50c5d
Valeriy <cherkasov77@mail.ru>
2022-10-09 12:38:25 +0400

    Sem4num2

--- Seminar4_2.py
+++ Seminar4_2.py
@@ -2,3 +2,11 @@
 import math
 b = abs(int(input('Введите число заначений:')))
 kod = []
+for i in range(2,b+1):
+    for j in  range(2,i):
+        if i%j==0:
+            break
+    else:
+            kod.append(i)
+            
+print(kod)
